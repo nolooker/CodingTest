fd263a79292e06c350850b37057e0b4f25e9857a
nolooker <121871676+nolooker@users.noreply.github.com>
2024-10-26 20:03:43 +0900

    Title: 두 사람, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 241026/두 사람/two-person.py
+++ 241026/두 사람/two-person.py
@@ -1,13 +1,18 @@
-a1,b1 = map(str, input().split())
-a2,b2 = map(str, input().split())
+a1,b1 = input().split()
+a2,b2 = input().split()
 
 a1 = int(a1)
 a2 = int(a2)
 
-if a1 >= 19:
-    if b1 == "M":
-        print(1)
-    else:
-        print(0)
+result = 0
+
+if a1 >= 19 and b1 == "M" and a2 >= 19 and b2 =="M":
+    result = 1
+elif a1 >= 19 and b1 == "M":
+    result = 1
+elif a2 >= 19 and b2 == "M":
+    result = 1
 else:
-    print(0)
+    result = 0
+
+print(result)
